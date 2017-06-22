a89894c25c205860dcab187e5fc95178eb12ecd9
CuriousFu <2269930627@qq.com>
2017-06-22 21:28:07 +0800

    modified:   config 	modified:   wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  find ~/Desktop/wallpaper -type f \
+  find ~/Pictures/Wallpapers -type f \
     -name '*.jpg' -print0 | shuf -n1 -z | xargs -0 feh --bg-scale
-  sleep 10m
+  sleep 8m
 done
